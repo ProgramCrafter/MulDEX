c95aa8bb6fe473ee7f67b1bf83a704d5f8fe2e10
Timur Kalmykov <lendedland@gmail.com>
2023-10-03 04:59:59 +0700

    contract deploy

--- scripts/deployMultitokenDex.ts
+++ scripts/deployMultitokenDex.ts
@@ -1,22 +1,14 @@
-import { toNano } from 'ton-core';
-import { MultitokenDex } from '../wrappers/MultitokenDex';
+import { Address, Cell, Dictionary, toNano } from 'ton-core';
+import { MultitokenDex, sendDeploy } from '../wrappers/MultitokenDex';
 import { NetworkProvider } from '@ton-community/blueprint';
+import { inspect } from 'util';
 
 export async function run(provider: NetworkProvider) {
-    const multitokenDex = provider.open(await MultitokenDex.fromInit());
+    const multitokenDex = provider.open(await MultitokenDex.fromInit(BigInt(1), provider.sender().address!));
 
-    await multitokenDex.send(
-        provider.sender(),
-        {
-            value: toNano('0.05'),
-        },
-        {
-            $$type: 'Deploy',
-            queryId: 0n,
-        }
-    );
+
+    await sendDeploy(multitokenDex, provider, ["EQAMcImLBgZHazWmradz51pI0uHZwvxMONlMQy0QwQTQInD5", "kQAbDiNBKXDn5l3AE8cx-j7zZneFITRRFM-FH-HfBJqsrTLh"].map(elem => Address.parse(elem)))
 
     await provider.waitForDeploy(multitokenDex.address);
-
     // run methods on `multitokenDex`
 }
--- wrappers/MultitokenDex.ts
+++ wrappers/MultitokenDex.ts
@@ -1,1 +1,15 @@
+import { Address, Dictionary, OpenedContract, TupleBuilder, toNano } from 'ton-core';
+import { MultitokenDex } from '../build/MultitokenDex/tact_MultitokenDex';
+import { NetworkProvider } from '@ton-community/blueprint';
+
 export * from '../build/MultitokenDex/tact_MultitokenDex';
+
+export async function sendDeploy(contract: OpenedContract<MultitokenDex>, provider: NetworkProvider, masters: Address[]) {
+    let dict: Dictionary<Address, Address> = Dictionary.empty();
+    let tuple = new TupleBuilder();
+    tuple.writeAddress(contract.address);
+    for (let item of masters) {
+        dict.set(item, (await provider.provider(item).get("get_wallet_address", tuple.build())).stack.readAddress());
+    }
+    await contract.send(provider.sender(), {value: toNano("0.1"), bounce: false}, {$$type: 'DexDeploy', query_id: 12n, jetton_wallets: dict});
+}
